49b346c8e4a263038b358955ec7b21c05e16352e
Daraujox <daniel_d.s@outlook.com>
2023-02-26 19:46:38 -0500

    se agrega el proyecto

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from './ping.controller';
+export * from './controlador.controller';
+export * from './producto.controller';
+export * from './item.controller';
+export * from './factura.controller';
---
+++ src/models/factura.model.ts
@@ -0,0 +1,21 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Factura extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  constructor(data?: Partial<Factura>) {
+    super(data);
+  }
+}
+
+export interface FacturaRelations {
+  // describe navigational properties here
+}
+
+export type FacturaWithRelations = Factura & FacturaRelations;
